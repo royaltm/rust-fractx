e5cb0f2d10a78d4cc81c740022227ca07956a6b0
Rafał Michalski <royaltm75@gmail.com>
2017-10-26 13:56:39 +0200

    zapedzilem sie

--- src/iter.rs
+++ src/iter.rs
@@ -1,7 +1,7 @@
 use super::fractal::MAXI;
 
 pub fn iter(x0: f64, y0: f64) -> bool {
-    let (mut x, mut y) : (f64, f64) = (x0, y0);
+    let (mut x, mut y) = (x0, y0);
 
     for _ in 1..MAXI {
         let (xx, yy) = (x*x, y*y);
