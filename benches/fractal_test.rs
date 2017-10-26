b61aec4e6f20fb5fbd2d30e2be4aa92c45a652d3
Rafał Michalski <royaltm75@gmail.com>
2017-10-26 14:53:37 +0200

    EOLs

---
+++ benches/fractal_test.rs
@@ -0,0 +1,41 @@
+#![feature(test)]
+
+extern crate test;
+extern crate image;
+
+extern crate fractx;
+use test::Bencher;
+use fractx::fractal::new_fractal;
+use image::{Rgb, Rgba, Luma, LumaA};
+
+#[bench]
+fn bench_rgba(ben: &mut Bencher) {
+    let img = new_fractal(700, 400, -2.5, -1.0, 1.0, 1.0);
+    ben.iter(|| {
+        img.to_img::<Rgba<u8>>();
+    });
+}
+
+#[bench]
+fn bench_rgb(ben: &mut Bencher) {
+    let img = new_fractal(700, 400, -2.5, -1.0, 1.0, 1.0);
+    ben.iter(|| {
+        img.to_img::<Rgb<u8>>();
+    });
+}
+
+#[bench]
+fn bench_luma(ben: &mut Bencher) {
+    let img = new_fractal(700, 400, -2.5, -1.0, 1.0, 1.0);
+    ben.iter(|| {
+        img.to_img::<Luma<u8>>();
+    });
+}
+
+#[bench]
+fn bench_luma_a(ben: &mut Bencher) {
+    let img = new_fractal(700, 400, -2.5, -1.0, 1.0, 1.0);
+    ben.iter(|| {
+        img.to_img::<LumaA<u8>>();
+    });
+}
